f0d485ca8ab3d6cab45fc675cc13ba52af234542
rainriver <rainriver5014@gmail.com>
2022-01-17 06:33:47 -0800

    set everyday price

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.0;
 
 contract Box {
     uint256 public x;
+    uint public price;
+
+    function setPrice(uint _price) external {
+        price = _price;
+    }
 
     function store(uint256 _x) external {
         x = _x;
